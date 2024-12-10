33c290f79e725723531671511d41d32c348c1375
stormymayday <stormymayday7@gmail.com>
2024-12-10 15:20:37 -0800

    removed daisyui config import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,5 +58,5 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate"), require("daisyui")],
+    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
